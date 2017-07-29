0227afb78b4e6797220d7b905c35a7a41fac4743
Zengyu Zeng <zayn@mail.ustc.edu.cn>
2017-07-29 18:25:47 +0800

    fix: typo

--- src/main/java/com/zayn/repo/CustomerRepository.java
+++ src/main/java/com/zayn/repo/CustomerRepository.java
@@ -1,6 +1,7 @@
 package com.zayn.repo;
 
 import com.zayn.bean.Customers;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +17,8 @@ import javax.transaction.Transactional;
 public interface CustomerRepository extends CrudRepository<Customers, Long> {
     @Query(value = "SELECT * FROM Customers WHERE serial_num = :serial_num", nativeQuery = true)
     Customers findBySerial_num(@Param("serial_num") String serial_num);
+
+    @Modifying
+    @Query(value = "UPDATE Customers pwd = :pwd WHERE serial_num = :serial_num", nativeQuery = true)
+    void updatePwd(@Param("serial_num") String serial_num, @Param("pwd") String pwd);
 }
